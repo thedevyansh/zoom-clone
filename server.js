ad3427c1d0284ad019528c5cae65fac7cf1e7b29
thedevyansh <schawla333333@gmail.com>
2020-10-05 18:35:51 +0530

    Added: funtionality to video call when a new user get connected

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -15,4 +21,11 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-server.listen(3030);
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
+});
+
+server.listen(3030, () => console.log("Server started on port 3030"));
